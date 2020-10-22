deb28db7eee65a4de2076209bfe370c96e39e0e4
deepanshuslicepay <deepanshu.garg@sliceit.com>
2020-10-22 20:12:07 +0530

    fix(comment): removed commented code

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,5 @@
-// import { equal } from 'assert';
 import getValue from '../src/index';
 import { assert } from 'console';
-
-// const a = 10;
 
 describe('Testing started using sinon for build folder(dist)', () => {
     it('function check', () => {
