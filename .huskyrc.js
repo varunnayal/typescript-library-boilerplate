96dbbb5b298dfbdee24d2adad99e0794b23a7661
slice-varunnayal <varun.nayal@sliceit.com>
2021-02-14 12:52:45 +0530

    feat: upgrade husky v4->v5, package.json updates

--- .huskyrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  hooks: {
-    'pre-commit': './node_modules/.bin/lint-staged -c ./.lintstaged.js',
-    'commit-msg': './node_modules/.bin/commitlint -E HUSKY_GIT_PARAMS',
-  },
-};
